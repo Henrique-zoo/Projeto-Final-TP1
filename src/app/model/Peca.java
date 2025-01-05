cc1b659b0a78a22f0350d82f1f67c24c1f9423fc
Nirva <nive.yellow@gmail.com>
2025-01-05 14:14:16 -0300

    Trabalhando nas funcionalidades das classes par e peca

--- src/app/model/Peca.java
+++ src/app/model/Peca.java
@@ -4,26 +4,80 @@
  */
 package app.model;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 /**
  *
  * @author nirva
  */
 public class Peca {
-	// attributes
+	
+	// static
+	// Array of registered parts
+	public static ArrayList<Peca> pecasCadastradas = new ArrayList<>();
+	
+	// id management
 	private static int id_generator = 0;
-	private int id;
+	// this is a simple queue wherein new parts use the oldest free id and new free ids get added to its end.
+	private static ArrayList<Integer> free_ids = new ArrayList<>();
+	
+	// attributes
+	private final int id;
 	private String tipo;
 	private String marca;
 	private Double preco;
 	
 	// constructor
 	public Peca(String tipo, String marca, Double preco) {
-		this.id = id_generator++;
+		if (!(free_ids.isEmpty())) {
+			this.id = free_ids.get(0);
+			free_ids.remove(0);
+		} else {
+			this.id = id_generator++;
+		}
 		this.tipo = tipo;
 		this.marca = marca;
 		this.preco = preco;
+		
+		// no leak, appending a reference to the list will be fine once object is done constructing
+		pecasCadastradas.add(this);
+		pecasCadastradas.sort(Comparator.comparing(a -> a.getId()));
 	}
 	
+	// static methods
+	
+	// removerCadastro: remove a peca cadastrada de acordo com o id
+	public static void removerCadastro(int id)
+	{
+		for (Peca pecaCadastrada : pecasCadastradas) {
+			if (pecaCadastrada.id == id) {
+				pecasCadastradas.remove(pecaCadastrada);
+				free_ids.add(id);
+			}
+		}
+	}
+	
+	// removerCadastro: remove a peca cadastrada de acordo com tipo e marca
+	public static void removerCadastro(String tipo, String marca) {
+		int id = -1;
+		Peca aux = null;
+		for (Peca pecaCadastrada : pecasCadastradas) {
+			if (pecaCadastrada.getMarca().equals(marca) && pecaCadastrada.getTipo().equals(tipo)) {
+				id = pecaCadastrada.id;
+				aux = pecaCadastrada;
+				break;
+			}
+		}
+		
+		if (id >= 0 && aux != null) {
+			pecasCadastradas.remove(aux);
+			free_ids.add(id);
+		}
+	}
+	
+	// getters
+
 	public int getId()
 	{
 		return id;
@@ -43,11 +97,8 @@ public class Peca {
 	{
 		return preco;
 	}
-
-	public void setId(int id)
-	{
-		this.id = id;
-	}
+	
+	// setters (id is always set by constructor and should never be changed)
 
 	public void setTipo(String tipo)
 	{
